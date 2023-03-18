db3f197887acf09b844c61a28f5529ba2c2cd536
Sih778 <126001796+Sih778@users.noreply.github.com>
2023-03-18 20:08:52 -0300

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,10 @@
+function Enviar() {
+          var nome = document.getElementById("nomeid");
+          var tel = document.getElementById("foneid");
+          var email = document.getElementById("emailid");
+          if (tel.value != "") {
+          alert('Obrigado sr(a) ' + nome.value + ' Os seus dados,   Tel: '+ tel.value + ' E-mail:' +
+          email.value + '. Foram enviados com Sucesso');
+          }
+        }
+      function Trocar() {
